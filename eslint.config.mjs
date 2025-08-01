b0ae712b034339d9fa35e10610cbe632b70bb561
Ady Aprilriyan Rizki <adyaprilriyanrizki@gmail.com>
2025-08-01 15:13:03 +0700

    fix 7

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals'),
   {
-    ignores: ["app/generated/prisma/**"],
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      '.vercel/**',
+      'public/**',
+    ],
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
